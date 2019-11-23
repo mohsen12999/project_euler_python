c5ccacbddce04dcc1fc9464c5801a523759cb352
Mohsen Shabanian <mohsen1299@gmail.com>
2019-11-24 01:11:13 +0330

    solve problem 10 - but answer make a lot of time

---
+++ problem10.py
@@ -0,0 +1,20 @@
+# https://projecteuler.net/problem=10
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+max=2000000
+
+def isPrime(number):
+    for i in range(2,int(number/2)+1):
+        if number % i ==0:
+            return False
+
+    return True
+
+sum=0
+for i in range(2,max):
+    if isPrime(i):
+        # print(i)
+        sum=sum+i
+
+print(sum)
