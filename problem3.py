7a58db99ba68c53ff08c662ca27038abd4220c4f
Mohsen Shabanian <mohsen1299@gmail.com>
2019-11-23 23:31:29 +0330

    problem 5

--- problem3.py
+++ problem3.py
@@ -1,5 +1,8 @@
-#The prime factors of 13195 are 5, 7, 13 and 29.
-#What is the largest prime factor of the number 600851475143 ?
+# https://projecteuler.net/problem=3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+num = 600851475143
 
 def isPrime(number):
     for i in range(2,int(number/2)):
@@ -7,16 +10,6 @@ def isPrime(number):
             return False
     
     return True
-
-num = 600851475143
-
-# for i in range(int(num/2),2,-1):
-#     if num % i == 0:
-#         factor = i
-#         print("factor "+i)
-#         if(isPrime(i)):
-#             print("answer is: "+i)
-#             break
 
 number = num
 i = 2
---
+++ problem5.py
@@ -0,0 +1,16 @@
+# https://projecteuler.net/problem=5
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+num=20
+answer=1
+
+for i in range(2,num+1):
+    for j in range(i,0,-1):
+        if i%j==0 and answer%j==0:
+            break
+    print(i,j,i/j)
+    answer=answer*i/j
+
+print(answer)
+# 232792560
